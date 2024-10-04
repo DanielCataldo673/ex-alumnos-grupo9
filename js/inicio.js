26ddfd4a5d0066fcc670305ab15313a40e627957
DanielCataldo673 <danielcataldo673@gmail.com>
2024-10-04 18:11:59 -0300

    first commit

--- js/inicio.js
+++ js/inicio.js
@@ -4,19 +4,18 @@ function startPresentation() {
     document.querySelector('.start').style.display = 'none';  
     document.querySelector('.animation').style.display = 'block';  
     
+    // Reproducir el audio  
+    const audio = document.getElementById('starWarsAudio');  
+    audio.currentTime = 0; // Reinicia el audio  
+    audio.play().catch(error => {  
+        console.log('Error al intentar reproducir audio:', error);  
+    });  
+    
     // Mostrar la sección intro  
     const introSection = document.querySelector('.intro');  
     introSection.style.display = 'block';  
     introSection.style.opacity = '1';  // Asegurarse de que se muestre completamente  
     
-    // Reproducir el audio   
-    const audio = document.getElementById('starWarsAudio');  
-
-    // Intentar reproducir el audio  
-    audio.play().catch(error => {  
-        console.log('Error al intentar reproducir audio:', error);  
-    });  
-
     // Mostrar la sección intro durante 3 segundos (o el tiempo que desees)  
     setTimeout(() => {  
         introSection.style.opacity = '0';  // Iniciar desvanecimiento  
@@ -29,14 +28,14 @@ function startPresentation() {
             const starWarsSection = document.querySelector('.star-wars');  
             starWarsSection.style.display = 'block';  
 
-            // Ocultar la sección star-wars después de 150 segundos y volver a la pantalla de inicio  
+            // Ocultar la sección star-wars después de 50 segundos y volver a la pantalla de inicio  
             setTimeout(() => {  
                 audio.pause();  
                 audio.currentTime = 0; // Reinicia el audio  
                 starWarsSection.style.display = 'none';  
                 document.querySelector('.animation').style.display = 'none';  
                 document.querySelector('.start').style.display = 'block'; // Regresa a la pantalla principal  
-            }, 50000); // Mostrar star-wars durante 150 segundos  
+            }, 50000); // Mostrar star-wars durante 50 segundos  
         }, 500); // Tiempo que toma el fade-out antes de ocultar  
     }, 3000); // Muestra la sección intro durante 3 segundos (ajusta según sea necesario)  
 }  
@@ -44,6 +43,8 @@ function startPresentation() {
 // Este bloque se asegura de que el script se ejecute después de que el DOM esté completamente cargado  
 document.addEventListener('DOMContentLoaded', () => {  
     // Seleccionamos el botón y le agregamos el evento de clic  
-    const presentacionBtn = document.querySelector('.presentacion-btn');  
-    presentacionBtn.addEventListener('click', startPresentation);  
+    const presentacionBtns = document.querySelectorAll('.presentacion-btn');  
+    presentacionBtns.forEach(btn => {  
+        btn.addEventListener('click', startPresentation);  
+    });  
 });
